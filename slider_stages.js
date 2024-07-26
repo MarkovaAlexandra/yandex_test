21b003cd73dab1a9c274a50e5243640783110f4a
Alexandra <alexandramarkova573@gmail.com>
2024-07-26 18:41:26 +0300

    finally

--- slider_stages.js
+++ slider_stages.js
@@ -1,15 +1,15 @@
 'use strict';
-console.log('slider stages');
+
 //слайды
 const slides = Array.from(document.querySelectorAll('[data-slider=yes]'));
-console.log(slides);
+
 //кружки в слайдере
 const circles_slide = Array.from(document.querySelectorAll('.slider-circle'));
-console.log(circles_slide);
+
 //кнопки переключения
 const btn_next = document.querySelector('.stages-next');
 const btn_prev = document.querySelector('.stages-back');
-console.log(btn_next);
+
 btn_next.addEventListener('click', nextSlide);
 btn_prev.addEventListener('click', prevSlide);
 
@@ -17,14 +17,11 @@ btn_prev.addEventListener('click', prevSlide);
 let slideNumber = 0;
 
 function nextSlide() {
-    console.log('я тут');
     slideNumber = (slideNumber + 1) % slides.length;
-    console.log(slideNumber);
     btn_prev.disabled = false;
     if (slideNumber === 4) {
         btn_next.disabled = true;
     }
-
     showSlide();
 }
 
@@ -34,7 +31,6 @@ function prevSlide() {
     if (slideNumber === 0) {
         btn_prev.disabled = true;
     }
-    console.log(slideNumber);
     showSlide();
 }
 
